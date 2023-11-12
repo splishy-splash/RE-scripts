0e27b93c9178aeb693f7270b497c40ff45cc6d47
splishy-splash <43074817+splishy-splash@users.noreply.github.com>
2023-11-12 01:32:41 -0500

    changed 'EIP' to ida_dbg.get_ip_val()
    this should fix 32-bit vs 64-bit errors. Also added initial logic for 'jmp' instructions.

--- run_until_call.py
+++ run_until_call.py
@@ -1,6 +1,11 @@
 def run_until_call():
-	while idc.print_insn_mnem(get_reg_value('EIP')) != 'call':
+	while True:
 		step_into()
 		code = idaapi.wait_for_next_event(idaapi.WFNE_CONT | idaapi.WFNE_SUSP, -1)
 		if code <= 0:
 			return (False, "Failed to run until call.")
+		elif idc.print_insn_mnem(ida_dbg.get_ip_val()) == 'call':
+			return
+		elif idc.print_insn_mnem(ida_dbg.get_ip_val()) == 'jmp':
+			if idc.get_operand_type(idc.print_insn_mnem(ida_dbg.get_ip_val()) not in [6,7]:
+				return
